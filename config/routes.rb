944873a14f62aade7fafc5462c6ad206fd9529ef
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2026-04-24 00:23:34 +0900

    devise styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
